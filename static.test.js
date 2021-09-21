8f909b5cdc74e80fc3662d9573484f2460116dbc
Cristian Montes <cmontesmujica@gmail.com>
2021-09-20 22:41:30 -0700

    static test finally passin

--- static.test.js
+++ static.test.js
@@ -1,6 +1,8 @@
 import request from 'supertest';
 import { readFile } from 'fs/promises';
 import app from './lib/app.js';
+import jest from 'jest'
+
 
 describe('get static file routes', () => {
   it('should return index.html from GET /', async () => {
@@ -12,8 +14,9 @@ describe('get static file routes', () => {
   });
 
   it('should return main.css from GET /', async () => {
+  
     const [res, index]  = await Promise.all([
-      request(app).get('/css'),
+      request(app).get('/css/main.css'),
       readFile('./public/css/main.css', 'utf-8')
     ]);
     expect(res.text).toEqual(index);
